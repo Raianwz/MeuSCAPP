4aadc0df1ff29de12d4646f632d1771f57a05cc2
raianwz <raian03@live.com>
2023-12-03 15:10:09 -0300

    Ajustes na Custom 'StatusBar'

--- src/components/statusBar/index.js
+++ src/components/statusBar/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 
-export default function StatusBarColor() {
+export default function StatusBarColor({cor, estilo}) {
+    cor = cor ?? '#289E10'
+    estilo = estilo ?? 'light'
     return (
-        <StatusBar backgroundColor="#289E10" barStyle='light-content' style='light' />
+        <StatusBar backgroundColor={cor} barStyle={estilo+'-content'} style={estilo} />
     )
 }
 
